637e4b24469ae19d70e27f440427aded62584c77
song <hellsxs@163.com>
2020-09-06 11:40:16 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import lodash form 'lodash'
-import moment form 'moment'
+// import lodash form 'lodash'
+// import moment form 'moment'
 
 Vue.config.productionTip = false
 
