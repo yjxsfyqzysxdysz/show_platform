f7485d7153bcc8a97adcf44ba825108fd7b311a0
song <hellsxs@163.com>
2020-09-29 18:38:02 +0800

    创建其余子组件

---
+++ src/views/project2/revision2.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="revision2" :style="{'background-color': colorInfo.HEX}">
+    <div class="left"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+import colordata from './colorData.json'
+import annulus from '@com/canvas/annulus'
+import barBhart from '@com/canvas/barBhart'
+
+export default {
+  name: 'revision2',
+  components: {
+    annulus,
+    barBhart
+  },
+  created() {
+    this.colordata = colordata
+  },
+  data() {
+    return {
+      colordata: [],
+      colorInfo: {
+        cmyk: [],
+        rgb: [],
+        HEX: '#64363C',
+        mainText: '紅梅',
+        subText: 'KOHBAI'
+      },
+    }
+  },
+  methods: {
+    filterTextColor(index) {
+      let color = ''
+      switch (index) {
+        case 0:
+          color = '#0093d3'
+          break;
+        case 1:
+          color = '#cc006b'
+          break;
+        case 2:
+          color = '#fff10c'
+          break;
+        case 3:
+          color = '#000000'
+          break;
+        default:
+          color = '#ffffff'
+          break;
+      }
+      return color
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.revision2 {
+}
+</style>
---
+++ src/views/project2/revision4.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="revision4">
+revision4
+  </div>
+</template>
+
+<script>
+import colordata from './colorData.json'
+
+export default {
+  name: 'revision4',
+  created() {
+    this.colordata = colordata
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.revision4 {
+}
+</style>
